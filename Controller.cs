589682164d328c1e5b53848616a9155ae0037a01
HonlightStudio <196152029+HonlightStudio@users.noreply.github.com>
2025-11-29 17:06:26 +0330

    v2

--- Controller.cs
+++ Controller.cs
@@ -1,18 +1,63 @@
+using System;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+
     public CharacterController characterController;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 12f;
+    public Gun gun;
+    private Camera m_Cam;
+    private InputSystem_Actions m_InputSystemActions;
+    
+    private float m_Yaw;
+    private float m_Pitch;
+    
+    private float yaw;
+    private float pitch;
+    
+    void OnEnable()
     {
-        
+
+        m_InputSystemActions.Enable();
     }
+
+    void OnDisable()
+    {
+        m_InputSystemActions.Disable();
+    }
+
+    
+    
+    void Awake()
+    {
+        m_Cam = Camera.main;
+        m_InputSystemActions = new InputSystem_Actions();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        m_InputSystemActions.Player.Attack.started += context => gun.Shoot();
+    }
+
+  
 
     // Update is called once per frame
     void Update()
     {
-        characterController.Move(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime);
-        characterController.Move(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime);
+        Vector2 look = m_InputSystemActions.Player.Look.ReadValue<Vector2>();
+        
+        
+        pitch -= look.y; 
+        yaw += look.x;
+        pitch = Mathf.Clamp(pitch, -90, 90);
+        transform.rotation = Quaternion.Euler(0, yaw, 0);
+        m_Cam.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+        Vector3 move = transform.forward * m_InputSystemActions.Player.Move.ReadValue<Vector2>().y+
+                       transform.right * m_InputSystemActions.Player.Move.ReadValue<Vector2>().x;
+        
+        move = move.normalized * moveSpeed * Time.deltaTime;
+        characterController.Move(move);
+
     }
+
+    
 }
---
+++ Gun.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public RectTransform aim;
+    public float range;
+    private Camera mainCamera;
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    public void Shoot()
+    {
+        Vector3 aimpos = mainCamera.ScreenToWorldPoint(aim.position);
+        bool hited = Physics.Raycast(aimpos, mainCamera.transform.forward, out RaycastHit hit,range);
+        if (hited)
+        {
+            Debug.unityLogger.Log(hit.collider.name);
+        }
+    }
+}
